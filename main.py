d219ca3cb07dae00a9058a220d586286de79f1ac
MageInTraining <stehlik211@gmail.com>
2021-09-11 22:40:20 +0200

    all turorial done

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def scrape_product_detail_page(product_detail_url):
 
 
 def real_python_exercise_beautiful_soup(base_url):
-    from urllib import urlopen
+    import urllib as ul
     from bs4 import BeautifulSoup
 
-    html_page = urlopen(base_url + "/profiles")
+    html_page = ul.urlopen(base_url + "/profiles")
     html_text = html_page.read().decode("utf-8")
     soup = BeautifulSoup(html_text, "html.parser")
 
@@ -37,9 +37,26 @@ def real_python_exercise_mechanical_soup(url):
         address = link["href"]
         text = link.text
         print(f"{text}: {address}")
+    title = profiles_page.soup.title
+    print(title)
+
+def real_python_exercise_mechanical_soup2():
+    import mechanicalsoup as ms
+    import time
+    browser = ms.Browser()
+
+    for i in range(4):
+        page = browser.get("http://olympus.realpython.org/dice")
+        tag = page.soup.select("#result")[0]
+        result = tag.text
+        print(f"The result of your dice roll is: {result}")
+        if i < 3:
+            time.sleep(2)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # print_hi('PyCharm')
     #real_python_exercise_beautiful_soup("http://olympus.realpython.org")
-    real_python_exercise_mechanical_soup("http://olympus.realpython.org/login")
+    #real_python_exercise_mechanical_soup("http://olympus.realpython.org/login")
+    #real_python_exercise_mechanical_soup2()
+    scrape_product_detail_page()
