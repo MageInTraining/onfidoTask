81af0d7a8c8827fe78c245675fdf649491e5d978
MageInTraining <stehlik211@gmail.com>
2021-09-11 08:09:16 +0200

    init

--- main.py
+++ main.py
@@ -3,15 +3,28 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import urllib as ul
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Hi, {0}".format(name))  # Press Ctrl+F8 to toggle the breakpoint.
 
+def scrape_product_detail_page(product_detail_url):
+    pass
+
+def real_python_exercise(url):
+    page = ul.urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    title_index = html.find("<title>")
+    start_index = title_index + len("<title>")
+    end_index = html.find("</title>")
+    title = html[start_index:end_index]
+    
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharmmmm')
+    #print_hi('PyCharm')
+    real_python_exercise("http://olympus.realpython.org/profiles")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
