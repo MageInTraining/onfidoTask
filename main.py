84b97fcdf97469b725a92136ea8fc9a0c57783a2
MageInTraining <stehlik211@gmail.com>
2021-09-12 11:06:53 +0200

    mode1

--- main.py
+++ main.py
@@ -1,62 +1,48 @@
 # coding=utf-8
 
-def print_hi(name):
-    print("Hi, {0}".format(name))
-
-
 def scrape_product_detail_page(product_detail_url):
-    pass
-
-
-def real_python_exercise_beautiful_soup(base_url):
-    import urllib as ul
+    import urllib.request
     from bs4 import BeautifulSoup
+    import pandas as pd
+    product_detail = {
+            "model": None
+            , "url": None
+            , "main_photo_path": None
+            , "additional_photo_paths": []
+            , "price": None
+            , "model_year": None
+            , "parameters": {
+                "weight": None
+                , "frame": None
+            }
+        }
 
-    html_page = ul.urlopen(base_url + "/profiles")
+    html_page = urllib.request.urlopen(product_detail_url)
     html_text = html_page.read().decode("utf-8")
     soup = BeautifulSoup(html_text, "html.parser")
 
-    for link in soup.find_all("a"):
-        link_url = base_url + link["href"]
-        print(link_url)
+    product_detail["model"] = soup.title.string
+    product_detail["url"] = product_detail_url
+    product_detail["main_photo_path"] = soup.find("img", id="nahled")["src"]
+    try:
+        pohledy = soup.find("div", class_="pohledy")
+        children = pohledy.findChildren("img")
+        for child in children:
+            product_detail["additional_photo_paths"].add(child["src"])
+    except:
+        pass
+
+    price = soup.find("div", class_="cena")
+    children = price.find("span")
+    product_detail["price"] = int(''.join(i for i in children.string if i.isdigit()))
+
+    dfs = pd.read_html(product_detail_url)
+    df = pd.concat(dfs)
+    product_detail["model_year"] = (df[df[1] == "Ročník"][2].item())
+    product_detail["parameters"]["weight"] = (df[df[1] == "Hmotnost"][2].item())
+    product_detail["parameters"]["frame"] = df[df[1] == "Rám"][2].item()
 
 
-def real_python_exercise_mechanical_soup(url):
-    import mechanicalsoup as ms
-    browser = ms.Browser()
-    login_page = browser.get(url)
-    login_html = login_page.soup
-
-    form = login_html.select("form")[0]
-    form.select("input")[0]["value"] = "zeus"
-    form.select("input")[1]["value"] = "ThunderDude"
-
-    profiles_page = browser.submit(form, login_page.url)
-    links = profiles_page.soup.select("a")
-    for link in links:
-        address = link["href"]
-        text = link.text
-        print(f"{text}: {address}")
-    title = profiles_page.soup.title
-    print(title)
-
-def real_python_exercise_mechanical_soup2():
-    import mechanicalsoup as ms
-    import time
-    browser = ms.Browser()
-
-    for i in range(4):
-        page = browser.get("http://olympus.realpython.org/dice")
-        tag = page.soup.select("#result")[0]
-        result = tag.text
-        print(f"The result of your dice roll is: {result}")
-        if i < 3:
-            time.sleep(2)
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print_hi('PyCharm')
-    #real_python_exercise_beautiful_soup("http://olympus.realpython.org")
-    #real_python_exercise_mechanical_soup("http://olympus.realpython.org/login")
-    #real_python_exercise_mechanical_soup2()
-    scrape_product_detail_page()
+    # scrape_product_detail_page("https://www.lapierre-bike.cz/produkt/spicy-cf-69/5943")
+    print(scrape_product_detail_page("https://www.lapierre-bike.cz/produkt/overvolt-glp-team-b500/5957"))
